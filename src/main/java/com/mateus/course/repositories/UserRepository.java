9e4174bbdb983b103ec0ae9051a1ae9d3efa3dba
Mateus Bastos <eluire@gmail.com>
2021-05-25 08:37:55 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/mateus/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mateus.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mateus.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+}
