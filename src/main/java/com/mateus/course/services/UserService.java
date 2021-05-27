49793c9c6f041bdca587e40410ad732ad16a6eb6
Mateus Bastos <eluire@gmail.com>
2021-05-27 09:18:16 -0300

    User update

--- src/main/java/com/mateus/course/services/UserService.java
+++ src/main/java/com/mateus/course/services/UserService.java
@@ -18,19 +18,33 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	public User findById( Long id) {
-		
+
+	public User findById(Long id) {
+
 		Optional<User> obj = repository.findById(id);
-		
+
 		return obj.get();
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-}
+
+	public User update(Long id, User user) {
+		User entity = repository.getOne(id);
+		updateData(entity, user);
+		return repository.save(entity);
+
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+		
+	}
+};
